2917ad0df54c312e0e31f4f8b84506e959a27099
AB <lucenelletenebre@alice.it>
2021-08-01 17:17:08 +0200

    Added index.html and website.js

---
+++ docs/website.js
@@ -0,0 +1,21 @@
+$(document).ready(function() { 
+
+    console.log('hello world2');
+
+    // button for calculating PropsSI
+    $('.Calc_PropsSI').click( function() {
+        console.log('Pressed Calc PropsSI 5');
+
+        var $box = $(this).closest('.PropsSI');
+        console.log($box);
+
+        // var out = $input.text()
+
+        // console.log(out);
+        
+    });
+
+
+
+
+});//$(document).ready(function()
